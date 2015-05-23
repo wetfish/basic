f3810b6e005734f224920a759812edddf89699b1
Rachel Fish <itsrachelfish@gmail.com>
2015-05-22 23:21:40 -0600

    Watch for changes...

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,25 @@ var replace = require('gulp-replace');
 var insert = require('gulp-insert');
 var minimist = require('minimist');
 var extend = require('util')._extend;
+var watch = require('gulp-watch');
 
 gulp.task('default', function()
 {
-    var files =
-    [
-        './src/_basic.js',      // Load the constructor first
-        './src/*.js',           // Then load everything else
-    ];
-    
-    gulp.src(files)
-    .pipe(concat('basic.js'))
-    .pipe(replace(/\n(.)/g, '\n    $1'))        // Make sure everything is indented nicely
-    .pipe(insert.prepend('(function()\n{'))     // Prepend singleton opening
-    .pipe(insert.append('})();'))               // Append singleton closing
-    .pipe(gulp.dest('./dist'));
+    watch('src/*.js', function ()
+    {
+        var files =
+        [
+            './src/_basic.js',      // Load the constructor first
+            './src/*.js',           // Then load everything else
+        ];
+        
+        gulp.src(files)
+        .pipe(concat('basic.js'))
+        .pipe(replace(/\n(.)/g, '\n    $1'))        // Make sure everything is indented nicely
+        .pipe(insert.prepend('(function()\n{'))     // Prepend singleton opening
+        .pipe(insert.append('})();'))               // Append singleton closing
+        .pipe(gulp.dest('./dist'));
+    });
 });
 
 gulp.task('build', function()
