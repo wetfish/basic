ec9fc2a6af483bdc99595e3cdd9ca37d399a7e48
Rachel Fish <itsrachelfish@gmail.com>
2015-05-28 15:17:30 -0600

    .style()

--- dist/basic.js
+++ dist/basic.js
@@ -281,4 +281,23 @@
         // Otherwise, return an array of sizes
         return output;
     }
+
+    ////////////////////////////////
+    // style() - set styles for matched elements
+    // usage - $('a').style({'color': 'red', 'font-size': '30pt'});
+
+    public.prototype.style = function(style)
+    {
+        var keys = Object.keys(style);
+
+        this.forEach(keys, function(key, property)
+        {
+            this.forEach(this.elements, function(index, element)
+            {
+                element.style[property] = style[property];
+            });
+        });
+            
+        return this;
+    }
 })();
---
+++ src/style.js
@@ -0,0 +1,18 @@
+////////////////////////////////
+// style() - set styles for matched elements
+// usage - $('a').style({'color': 'red', 'font-size': '30pt'});
+
+public.prototype.style = function(style)
+{
+    var keys = Object.keys(style);
+
+    this.forEach(keys, function(key, property)
+    {
+        this.forEach(this.elements, function(index, element)
+        {
+            element.style[property] = style[property];
+        });
+    });
+        
+    return this;
+}
