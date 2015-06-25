8e2afee4316a726bb7ac466564bb8b4248be5602
Rachel Fish <itsrachelfish@gmail.com>
2015-06-25 00:02:18 -0600

    .value() function for getting and setting input values - Fixes issue #15

--- dist/basic.js
+++ dist/basic.js
@@ -761,6 +761,45 @@
         });
     }
 
+    ////////////////////////////////
+    // value()                  - get the value of an input on all matched elements
+    // value('something')       - set the value of an input on all matched elements
+    // usage - $('textarea').value('this is some text!');
+
+    public.prototype.value = function(value)
+    {
+        var output = [];
+        
+        // Loop through current elements
+        this.forEach(this.elements, function(index, element)
+        {
+            // If no value is specified, return the current value of the input
+            if(value === undefined)
+            {
+                output.push(element.value);
+            }
+            // Otherwise, set the value 
+            else
+            {
+                element.value = value;
+            }
+        });
+
+        if(output.length)
+        {
+            // If only one element was matched, return that value
+            if(output.length == 1)
+            {
+                return output[0];
+            }
+
+            // Otherwise return an array of matched values
+            return output;
+        }
+
+        return this;
+    }
+
     // Depends on: private.width
 
     ////////////////////////////////
