b200fb59d47c1ec2a1466c912babe708919e8c12
Rachel Fish <itsrachelfish@gmail.com>
2015-07-24 23:58:16 -0600

    Rewrote hasClass to match multiple classes, partially fixes #20

--- dist/basic.js
+++ dist/basic.js
@@ -381,17 +381,30 @@
     // removeClass() - remove a class from all matched nodes
     // usage - if($('.selector').hasClass('example')) { console.log('wow!'); }
 
-    public.prototype.hasClass = function(className)
+    public.prototype.hasClass = function(classes)
     {
+        var classes = classes.split(' ');
         var match = false;
 
         // TODO: Break loop when match is found?
         this.forEach(this.elements, function(index, element)
         {
-            var classes = element.className.split(' ');
-            var index = classes.indexOf(className);
+            // Reset matches between each loop
+            var matches = {};
 
-            if(index != -1)
+            this.forEach(classes, function(index, className)
+            {
+                var classNames = element.className.split(' ');
+                var index = classNames.indexOf(className);
+
+                if(index != -1)
+                {
+                    matches[className] = true;
+                }
+            });
+
+            // Make sure we matched all of the requested classes
+            if(Object.keys(matches).length == classes.length)
             {
                 match = true;
             }
