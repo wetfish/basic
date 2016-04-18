9ee82dcbf8fc80296f09f13ae8c0bf81d39f83f4
Rachel Fish <itsrachelfish@gmail.com>
2016-04-18 17:18:14 -0600

    Added test for setting input values

--- test/value.js
+++ test/value.js
@@ -1,8 +1,14 @@
-QUnit.test("getting form value", function(assert)
+QUnit.test("Getting input value", function(assert)
 {
-    var single = $('input.one').value();
+    var single = $('input.get.one').value();
     assert.equal(single, 'Great test', 'Single value matched');
 
-    var double = $('input.one, input.two').value();
+    var double = $('input.get').value();
     assert.deepEqual(double, ['Great test', 'Another test'], 'Multiple values matched');
+});
+
+QUnit.test("Setting input value", function(assert)
+{
+    $('input.set').value('Sealed forever');
+    assert.equal(document.querySelector('input.set').value, 'Sealed forever', 'Value set');
 });
