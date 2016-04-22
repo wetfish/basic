1315f11a491f55b1fad0f8929ff2b0c80f98379f
Rachel Fish <itsrachelfish@gmail.com>
2016-04-21 20:05:32 -0600

    Added a test for .forEach()

--- test/_basic.js
+++ test/_basic.js
@@ -3,3 +3,17 @@ QUnit.test("Basic selectors", function(assert)
     assert.equal($('#selector').el[0], document.getElementById('selector'), 'Element selected by ID');
     assert.deepEqual($('.selector').el, document.querySelectorAll('.selector'), 'Elements selected by class');    
 });
+
+QUnit.test("Other basic functions", function(assert)
+{
+    var input = [1, 2, 3];
+    var output = [];
+
+    var basic = new $;
+    basic.forEach(input, function(value, index)
+    {
+        output[index] = value;
+    });
+
+    assert.deepEqual(input, output, '.forEach()');
+});
