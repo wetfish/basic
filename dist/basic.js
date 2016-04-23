0c40b4a3b50cadac4974b2ca81595d2a2deba024
Rachel Fish <itsrachelfish@gmail.com>
2016-04-23 17:08:28 -0600

    Updated .addClass() properly handle multiple classes at once

--- dist/basic.js
+++ dist/basic.js
@@ -183,21 +183,27 @@
     // addClass() - add a class to all matched nodes
     // usage - $('.selector').addClass('example');
 
-    public.prototype.addClass = function(className)
+    public.prototype.addClass = function(classNames)
     {
+        classNames = classNames.split(' ');
+
         this.forEach(this.elements, function(element)
         {
             var classes = element.className.split(' ');
-            var index = classes.indexOf(className);
 
-            // Only add a class if it doesn't exist
-            if(index == -1)
+            classNames.forEach(function(className)
             {
-                classes.push(className);
-                element.className = classes.join(' ');
-            }
+                var index = classes.indexOf(className);
+
+                // Only add a class if it doesn't exist
+                if(index == -1)
+                {
+                    classes.push(className);
+                    element.className = classes.join(' ');
+                }
+            });
         });
-                
+
         return this;
     }
 
