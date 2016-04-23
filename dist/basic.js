69ccc403ed479eadff9aa3e17722233b15eb0d0a
Rachel Fish <itsrachelfish@gmail.com>
2016-04-23 17:09:58 -0600

    Applied same update to .removeClass(), fixes broken test!

--- dist/basic.js
+++ dist/basic.js
@@ -835,19 +835,25 @@
     // removeClass() - remove a class from all matched nodes
     // usage - $('.selector').removeClass('example');
 
-    public.prototype.removeClass = function(className)
+    public.prototype.removeClass = function(classNames)
     {
+        classNames = classNames.split(' ');
+
         this.forEach(this.elements, function(element)
         {
             var classes = element.className.split(' ');
-            var index = classes.indexOf(className);
 
-            // Only remove a class if it exists
-            if(index != -1)
+            classNames.forEach(function(className)
             {
-                classes.splice(index, 1);
-                element.className = classes.join(' ');
-            }
+                var index = classes.indexOf(className);
+
+                // Only remove a class if it exists
+                if(index != -1)
+                {
+                    classes.splice(index, 1);
+                    element.className = classes.join(' ');
+                }
+            });
         });
 
         return this;
