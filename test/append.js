74ebdce1fe474c1cd3fb0a38e1481b3fcbd2d7b1
Rachel Fish <itsrachelfish@gmail.com>
2016-10-07 08:33:15 -0600

    Updated .append() tests to check if the content was actually appended

--- test/append.js
+++ test/append.js
@@ -13,12 +13,15 @@ QUnit.test("Appending strings and elements", function(assert)
     $('.append').append('appending a string!');
     el.dispatchEvent(evt);
     assert.equal(output.length, 1, 'Check if event remains bound after appending a string');
+    assert.equal($('.append').el[0].textContent.match('appending a string').length, 1, 'Check if the string was actually appended');
 
     $('.append').append('<u>Appending some HTML</u>');
     el.dispatchEvent(evt);
     assert.equal(output.length, 2, 'Check if event remains bound after appending html');
+    assert.equal($('.append').el[0].textContent.match('Appending some HTML').length, 1, 'Check if the HTML was actually appended');
 
     $('.append').append(appendBtn);
     el.dispatchEvent(evt);
     assert.equal(output.length, 3, 'Check if event remains bound after appending an element');
+    assert.equal($('.append button').el.length, 2, 'Check if the button was actually appended');
 });
